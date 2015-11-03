bc5e2f47333b1b2d520a07009cf79b3a9f7d9540
Kelly Hallman <khallman@tapmodo.com>
2015-11-03 11:03:34 -0800

    Setup "npm test"

---
+++ test/01_parsing.js
@@ -0,0 +1,15 @@
+
+describe('Parses RFC Examples',function(){
+  it('parse example1');
+  it('parse example2');
+  it('parse example3');
+  it('parse example4');
+  it('parse example5');
+  it('parse example6');
+  it('parse example7');
+});
+
+describe('Object Output',function(){
+  it('construct Record object');
+  it('write more tests');
+});
